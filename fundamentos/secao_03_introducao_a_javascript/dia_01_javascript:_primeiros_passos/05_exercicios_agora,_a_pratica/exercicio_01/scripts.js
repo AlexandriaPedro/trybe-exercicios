da9fb86eb13e5660400580d8490b4580ddefc333
Pedro Alexandria <alexandriapedroo@gmail.com>
2023-02-19 11:42:47 -0300

    ate o exercicio 3 dos exercicios do dia 01

---
+++ fundamentos/secao_03_introducao_a_javascript/dia_01_javascript:_primeiros_passos/05_exercicios_agora,_a_pratica/exercicio_01/scripts.js
@@ -0,0 +1,10 @@
+let num1 = 2;
+let num2 = 1;
+
+if (num1 > num2) {
+  console.log("O num1 é maior!")
+} else if(num1 < num2) {
+  console.log("O num2 é maior!")
+} else {
+  console.log("O num1 e num2 são iguais!")
+}
---
+++ fundamentos/secao_03_introducao_a_javascript/dia_01_javascript:_primeiros_passos/05_exercicios_agora,_a_pratica/exercicio_02/scripts.js
@@ -0,0 +1,13 @@
+let num1 = 1;
+let num2 = 2;
+let num3 = 3;
+
+if (num1 > num2 && num1 > num3) {
+  console.log("O num1 é maior!");
+} else if (num2 > num1 && num2 > num3) {
+  console.log("O num2 é maior!");
+} else if (num3 > num1 && num3 > num2) {
+  console.log("O num3 é maior!");
+} else {
+  console.log("O num1, num2 e num3 são iguais!");
+}
