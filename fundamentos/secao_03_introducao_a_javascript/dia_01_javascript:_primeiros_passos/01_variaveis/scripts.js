11202b522696e1dfc8db31033282d5b9faada224
Pedro Alexandria <alexandriapedroo@gmail.com>
2023-02-19 00:19:32 -0300

    exercicio 02 dos tipos primitivos e etc

--- fundamentos/secao_03_introducao_a_javascript/dia_01_javascript:_primeiros_passos/01_variaveis/scripts.js
+++ fundamentos/secao_03_introducao_a_javascript/dia_01_javascript:_primeiros_passos/01_variaveis/scripts.js
@@ -1,5 +1,5 @@
-let myName = "Pedro Alexandria"
-let birthCity = "Betim"
-let birthYear = "2002"
+let myName = "Pedro Alexandria";
+let birthCity = "Betim";
+let birthYear = "2002";
 
-console.log(myName, birthCity, birthYear)
+console.log(myName, birthCity, birthYear);
