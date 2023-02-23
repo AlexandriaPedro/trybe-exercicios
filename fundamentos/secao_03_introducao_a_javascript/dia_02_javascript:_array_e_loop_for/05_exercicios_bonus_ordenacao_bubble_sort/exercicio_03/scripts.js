50a6050ee1e9c1467e4b088416771887bdbef6b1
Pedro Alexandria <alexandriapedroo@gmail.com>
2023-02-23 14:15:28 -0300

    exercício 03 de exercícios bonus

--- fundamentos/secao_03_introducao_a_javascript/dia_02_javascript:_array_e_loop_for/05_exercicios_bonus_ordenacao_bubble_sort/exercicio_03/scripts.js
+++ fundamentos/secao_03_introducao_a_javascript/dia_02_javascript:_array_e_loop_for/05_exercicios_bonus_ordenacao_bubble_sort/exercicio_03/scripts.js
@@ -1,9 +1,14 @@
-let num = 2;
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let newNumbers = [];
 
-if (num > 0) {
-  console.log("positive");
-} else if(num < 0) {
-  console.log("negative")
-} else {
-  console.log("zero")
+for (let index = 1; index <= numbers.length; index += 1) {
+  for (let secondIndex = index - 1; secondIndex < index; secondIndex += 1) {
+    if (index === numbers.length) {
+      newNumbers.push(numbers[index - 1] * 2);
+    } else {
+      newNumbers.push(numbers[index] * numbers[secondIndex]);
+    }
+  }
 }
+
+console.log(newNumbers);
