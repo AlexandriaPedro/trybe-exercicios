27c65cf44b8cd18d0b714dcaa242817e023a5588
Pedro Alexandria <alexandriapedroo@gmail.com>
2023-02-23 11:00:38 -0300

    exercício 02 de exercícios

--- fundamentos/secao_03_introducao_a_javascript/dia_02_javascript:_array_e_loop_for/04_exercicios_agora,_a_pratica/exercicio_02/scripts.js
+++ fundamentos/secao_03_introducao_a_javascript/dia_02_javascript:_array_e_loop_for/04_exercicios_agora,_a_pratica/exercicio_02/scripts.js
@@ -1,13 +1,8 @@
-let num1 = 1;
-let num2 = 2;
-let num3 = 3;
+let numero = 10;
+let fatorial = 1;
 
-if (num1 > num2 && num1 > num3) {
-  console.log("O num1 é maior!");
-} else if (num2 > num1 && num2 > num3) {
-  console.log("O num2 é maior!");
-} else if (num3 > num1 && num3 > num2) {
-  console.log("O num3 é maior!");
-} else {
-  console.log("O num1, num2 e num3 são iguais!");
+for (let fatorando = numero; fatorando >= 1; fatorando -= 1) {
+  fatorial *= fatorando;
 }
+
+console.log("O fatorial de", numero, "é:", fatorial);
