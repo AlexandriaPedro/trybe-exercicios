2b71d8f8ec95f932d5f052be0ebc3238d6314b09
Pedro Alexandria <alexandriapedroo@gmail.com>
2023-02-25 18:40:04 -0300

    exercício de adicionando novas chaves

--- fundamentos/secao_03_introducao_a_javascript/dia_04_javascript_objetos/03_adicionando_novas_chaves/scripts.js
+++ fundamentos/secao_03_introducao_a_javascript/dia_04_javascript_objetos/03_adicionando_novas_chaves/scripts.js
@@ -1,5 +1,27 @@
-let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+let student = {};
 
-for (let index = 0; index < groceryList.length; index += 1) {
-  console.log(groceryList[index])
-}
+function addProperty (object, newKey, newValue) {
+  object[newKey] = newValue;
+};
+
+newKey = "nome";
+newValue = "Pedro Alexandria";
+addProperty(student, newKey, newValue);
+
+newKey = "email";
+newValue = "emailficticio@gmail.com";
+addProperty(student, newKey, newValue);
+
+newKey = "telefone";
+newValue = "21 98144-3218 (also fic)";
+addProperty(student, newKey, newValue);
+
+newKey = "github";
+newValue = "AlexandriaPedro";
+addProperty(student, newKey, newValue);
+
+newKey = "linkedln";
+newValue = "Pedro Alexandria";
+addProperty(student, newKey, newValue);
+
+console.log(student);
