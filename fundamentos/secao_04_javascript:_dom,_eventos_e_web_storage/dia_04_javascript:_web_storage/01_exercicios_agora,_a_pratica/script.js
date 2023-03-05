f8beada2b0c0b8d3fff5d6368408333f02188fc8
Pedro Alexandria <alexandriapedroo@gmail.com>
2023-03-04 22:54:10 -0300

    LocalStorage applied finally

--- fundamentos/secao_04_javascript:_dom,_eventos_e_web_storage/dia_04_javascript:_web_storage/01_exercicios_agora,_a_pratica/script.js
+++ fundamentos/secao_04_javascript:_dom,_eventos_e_web_storage/dia_04_javascript:_web_storage/01_exercicios_agora,_a_pratica/script.js
@@ -1,25 +1,37 @@
 window.onload = () => {
   //Changing background color;
-  let backgroundColor = document.getElementById("corpse");
+  let text = document.getElementById("text");
 
   let arrayColorButtons = document.querySelectorAll("#backgroundColor>button");
   
-  function changeBackgroundColor(colorButton) {
-    let color = colorButton.target.innerText;
-    backgroundColor.style.backgroundColor = color;
+  function changeBackgroundColor(backgroundColorButton) {
+    let color = backgroundColorButton.target.innerText;
+    text.style.backgroundColor = color;
+
+    //Putting the setting chose in localStorage;
+    localStorage.setItem("backgroundColor", color);
   }
-  
+
+  function setBackgroundColor(color) {
+    text.style.backgroundColor = color
+  }
   
   for (let index = 0; index < arrayColorButtons.length; index += 1) {
     arrayColorButtons[index].addEventListener("click", changeBackgroundColor);
   }
 
   //Changing font color;
-  let text = document.getElementById("text");
   let arrayFontColorButtons = document.querySelectorAll("#fontColor>button");
 
-  function changeFontColor (colorButton) {
-    let color = colorButton.target.innerText;
+  function changeFontColor (fontColorButton) {
+    let color = fontColorButton.target.innerText;
+    text.style.color = color;
+
+    //Putting the setting chose in localStorage;
+    localStorage.setItem("fontColor", color);
+  }
+
+  function setFontColor(color) {
     text.style.color = color;
   }
 
@@ -33,6 +45,13 @@ window.onload = () => {
   function changeFontSize (sizeButton) {
     let size = sizeButton.target.innerText;
     text.style.fontSize = size;
+
+    //Putting the setting chose in localStorage;
+    localStorage.setItem("fontSize", size);
+  }
+
+  function setFontSize(size) {
+    text.style.fontSize = size;
   }
 
   for (index = 0; index < arrayFontSizeButtons.length; index += 1) {
@@ -44,6 +63,13 @@ window.onload = () => {
 
   function changeFontSpacing (spacingButton) {
     let spacing = spacingButton.target.innerText;
+    text.style.lineHeight = spacing;
+
+    //Putting the setting chose in localStorage;
+    localStorage.setItem("fontSpacing", spacing);
+  }
+
+  function setFontSpacing(spacing) {
     text.style.lineHeight = spacing;
   }
 
@@ -57,9 +83,47 @@ window.onload = () => {
   function changeFontType (typeButton) {
     let type = typeButton.target.innerText;
     text.style.fontFamily = type;
+
+    //Putting the setting chose in localStorage;
+    localStorage.setItem("fontType", type);
+  }
+
+  function setFontType(type) {
+    text.style.fontFamily = type
   }
 
   for (index = 0; index < arrayFontType.length; index += 1) {
     arrayFontType[index].addEventListener("click", changeFontType);
   }
+
+  //Putting settings on localStorage;
+  function initialize() {
+    let backgroundColor = localStorage.getItem("backgroundColor");
+    console.log(backgroundColor);
+    if (backgroundColor) {
+      setBackgroundColor(backgroundColor);
+    }
+
+    let fontColor = localStorage.getItem("fontColor");
+    if (fontColor) {
+      setFontColor(fontColor);
+    }
+
+    let fontSize = localStorage.getItem("fontSize");
+    if (fontSize) {
+      setFontSize(fontSize);
+    }
+
+    let fontSpacing = localStorage.getItem("fontSpacing");
+    if (fontSpacing) {
+      setFontSpacing(fontSpacing);
+    }
+
+    let fontType = localStorage.getItem("fontType");
+    if (fontType) {
+      setFontType(fontType);
+    }
+  }
+
+  initialize();
 }
